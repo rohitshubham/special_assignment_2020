d688feda034026a5f34235e27fce5c2c8854d323
Rohit Raj <rohit.raj@aalto.fi>
2020-05-14 17:18:50 +0300

    Added transmission time in metadata

--- src/vgg_inference.py
+++ src/vgg_inference.py
@@ -16,6 +16,8 @@ test_model.to(device)
 
 loaded_model_weights = torch.load(model_path)
 
+bandwidth = 6.71  # in Mbits per/second
+
 modified_weights = collections.OrderedDict()
 for layer_name, weights in loaded_model_weights.items():
     new_layer_name = layer_name.replace(".", "_", 1)
@@ -73,10 +75,12 @@ def detect_images(img):
             start_time_edge = time.time()
             out = test_model(out, start_layer=i, stop_layer=i)
             elapsed_time = time.time() - start_time_edge
-            print(f'{data[str(i+1)]} : {(out.element_size() * out.nelement())/1024/1024} MB, time = {elapsed_time} ')
+            size = (out.element_size() * out.nelement())/1024/1024  #in MB
+            print(f'{data[str(i+1)]} : {size} MB, time = {elapsed_time} ')
             layer_metadata[i+1] = {"layer_name": data[str(i+1)],
-                                   "size": (out.element_size() * out.nelement())/1024/1024,
-                                   "edge_time": elapsed_time}
+                                   "size": size,
+                                   "edge_time": elapsed_time,
+                                   "transmission_time": size/bandwidth}
 
         set_layer_metadata(layer_metadata, "layer_metadata")
         return out
@@ -91,14 +95,14 @@ def detect_images(img):
             elapsed_time = time.time() - start_time_edge
             print(f'{data[str(i+1)]} : time = {elapsed_time}')
             layer_metadata[i+1] = {"layer_name": data[str(i+1)],
-                                   "edge_time": elapsed_time}
+                                   "server_time": elapsed_time}
 
         set_layer_metadata(layer_metadata, "server_time")
         return out
 
     with torch.no_grad():
         if not skip_server_time_generation:
-            out = generate_edge_parameters(batch_t)
+            out = generate_edge_parameters(batch_t)            
         else:
             out = generate_server_time(batch_t)
     return out
